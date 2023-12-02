d88f67562a8edfe82b7ba3279ede50ba23594ff2
Шамиль Атауллин <shamil@MacBook-Air.local>
2023-12-02 19:57:25 +0300

    4

--- code/4/main.go
+++ code/4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
 	"sync"
@@ -23,20 +24,31 @@ func main() {
 	for i := 0; i < num; i++ {
 		wg.Add(1)
 		go func(i int) {
+			// Цикл существует, пока открыт канал
 			for job := range numChan {
 				fmt.Println(i, " worker - ", job)
 			}
+			fmt.Println(i, " worker stopped")
 			wg.Done()
 		}(i)
 	}
 
-	for i := 0; i < 1000; i++ {
-		numChan <- i
-	}
-	close(numChan)
-	wg.Wait()
-
+	// Перехватываем сигнал control+c
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
+	signal.Notify(sigs, syscall.SIGINT)
+
+	for {
+		select {
+		// Если сигнал получен, закрываем канал и завершаем горутины
+		case <-sigs:
+			{
+				close(numChan)
+				wg.Wait()
+				return
+			}
+		// Если сигнала нет, отправляем данные в канал
+		default:
+			numChan <- rand.Intn(100)
+		}
+	}
 }
